d7a425877a56c7c0cb356475aa1a57614a62f571
James Riley <rileyj20@LabImage2016-17>
2018-05-02 15:56:19 -0700

    Add skeleton for Player

---
+++ Player.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+class Player 
+{
+   String playerID;
+   int fame;
+   int money;
+   int rank;
+   boolean workStatus;
+   int rehearseBonus;
+   Room playerLocation;
+   
+   public void getPlayerID(){
+      return null;
+   }
+   
+   public void setPlayerID(){
+      return null;
+   }
+   
+   public void getFame(){
+      return null;
+   }
+   
+   public void setFame(){
+      return null;
+   }
+
+   public void getMoney(){
+      return null;
+   }
+   
+   public void setMoney(){
+      return null;
+   }
+   public void getRank(){
+      return null;
+   }
+   
+   public void setRank(){
+      return null;
+   }
+   public void getWorkSatus(){
+      return null;
+   }
+   
+   public void setWorkStatus(){
+      return null;
+   }
+
+   
+   private void rehearse(){
+      return null;
+   }
+   
+   public Room move(){
+      return null;
+   }
+   
+   private void act(){
+      return null;
+   }
+   
+   private void upgrade(){
+      return null;
+   }
+   
+}
