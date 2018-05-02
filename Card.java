a4e85cadc400500d931f5ae6947a4c32171817b2
Joseph Tully <tullyj2@wwu.edu>
2018-05-02 16:10:12 -0700

    added skeleton for Card class

---
+++ Card.java
@@ -0,0 +1,24 @@
+
+public class Card {
+
+  Role[] onCardRoles;
+  //Player[] players;
+  int budget;
+
+  //card constructor
+  public Card() {
+  }
+
+  public Boolean hasOnCard() {
+  }
+
+  public int getBudget() {
+
+  }
+
+  public Role[] getRoles() {
+
+  }
+
+
+}
