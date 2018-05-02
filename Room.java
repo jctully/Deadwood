9f2e7db3b0344bbc966c79aa59c7cccb43737171
Joseph Tully <tullyj2@wwu.edu>
2018-05-02 15:50:05 -0700

    added skeleton for Room class

---
+++ Room.java
@@ -0,0 +1,36 @@
+
+public class Room {
+
+  Card card;
+  Room[] adjacentRooms;
+  //Player[] playersInRoom;
+  int shots;
+  Role[] offCardRoles;
+  Boolean wrapped;
+
+  public void addPlayer(Player player) {
+  }
+
+  public Room[] getAdjacentRooms() {
+  }
+
+  public void setCard(Card card) {
+  }
+
+  public Boolean isWrapped() {
+  }
+
+  public int getShots() {
+  }
+
+  public Role[] getRoles(){
+  }
+
+  public Card getCard() {
+  }
+
+  public void completeShot() {
+  }
+
+
+}
