7489712e72c70596e2cff1a7d5bbfd25838230a7
Joseph Tully <tullyj2@wwu.edu>
2018-05-03 16:21:24 -0700

    added skeleton for Role class

---
+++ Role.java
@@ -0,0 +1,30 @@
+
+public class Role {
+
+  String title;
+  String line;
+  Boolean onCard;
+  Boolean occupied;
+  int rank;
+
+  public Role(String title, String line, int rank) {
+
+  }
+
+  public Boolean isOnCard() {
+
+  }
+
+  public int getRank() {
+
+  }
+
+  public String getTitle() {
+
+  }
+
+  public String getLine() {
+
+  }
+
+}
