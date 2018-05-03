86e4a67245539f49bec67af411c0460e5442ed35
rileyj20 <rileyj20@wwu.edu>
2018-05-03 16:28:29 -0700

    Replace Player.java

--- Player.java
+++ Player.java
@@ -12,58 +12,58 @@ class Player
    Room playerLocation;
    
    public void getPlayerID(){
-      return null;
+     
    }
    
    public void setPlayerID(){
-      return null;
+      
    }
    
    public void getFame(){
-      return null;
+      
    }
    
    public void setFame(){
-      return null;
+      
    }
 
    public void getMoney(){
-      return null;
+      
    }
    
    public void setMoney(){
-      return null;
+      
    }
    public void getRank(){
-      return null;
+     
    }
    
    public void setRank(){
-      return null;
+     
    }
    public void getWorkSatus(){
-      return null;
+      
    }
    
    public void setWorkStatus(){
-      return null;
+      
    }
 
    
    private void rehearse(){
-      return null;
+      
    }
    
    public Room move(){
-      return null;
+     
    }
    
    private void act(){
-      return null;
+     
    }
    
    private void upgrade(){
-      return null;
+     
    }
    
 }
