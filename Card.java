e5efffb8c7a367527b921287434b54864e90ede8
Joseph Tully <tullyj2@wwu.edu>
2018-05-03 16:33:17 -0700

    updated Card class skeleton

--- Card.java
+++ Card.java
@@ -1,15 +1,27 @@
 
 public class Card {
 
+  String title;
+  String sceneNum;
   Role[] onCardRoles;
   //Player[] players;
   int budget;
 
   //card constructor
-  public Card() {
+  public Card(String title, String sceneNum, Role[] roles, int budget) {
+
   }
 
   public Boolean hasOnCard() {
+    
+  }
+
+  public String getTitle() {
+
+  }
+
+  public String getSceneNum() {
+
   }
 
   public int getBudget() {
