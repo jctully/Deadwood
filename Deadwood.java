d8d9ccc7fe5389b01d67507c3b0d979c10a63b39
Joseph Tully <tullyj2@wwu.edu>
2018-05-03 17:02:46 -0700

    added basic skeleton for Deadwood class

---
+++ Deadwood.java
@@ -0,0 +1,27 @@
+
+public class Deadwood {
+  int currentDay;
+  Player[] players;
+
+  public Player getCurrPlayer() {
+
+  }
+
+  public void nextTurn() {
+
+  }
+
+  public void endDay() {
+
+  }
+
+  public void endGame() {
+
+  }
+
+  public void main() {
+
+  }
+
+
+}
