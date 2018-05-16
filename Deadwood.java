9b3096409d1813d5b5d04121889d1d9787dba00b
Joseph Tully <tullyj2@wwu.edu>
2018-05-15 17:21:12 -0700

    added more method bodies to Deadwood class

--- Deadwood.java
+++ Deadwood.java
@@ -2,17 +2,24 @@
 public class Deadwood {
   int currentDay;
   Player[] players;
+  Player currentPlayer;
+  int currentPlayerNum;
 
   public Player getCurrPlayer() {
-    return null;
+    return this.currentPlayer;
   }
 
   public void nextTurn() {
-
+    this.currentPlayerNum = (this.currentPlayerNum+1)%4;
+    this.currentPlayer = this.players[this.currentPlayerNum];
   }
 
   public void endDay() {
+    Board.clearBoard();
+    for (Player p : this.players) {
+      // p.move(trailer);
 
+    }
   }
 
   public void endGame() {
