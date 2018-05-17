f10da9e14e6b813ddc5c670d9aaa9118378358d9
Joseph Tully <tullyj2@wwu.edu>
2018-05-16 17:27:30 -0700

    added Board Import from XML

--- BoardImport.java
+++ BoardImport.java
@@ -12,7 +12,7 @@ import java.io.File;
 public class BoardImport {
 
   // returns a Document object after loading the book.xml file.
-  public static Document getDocFromFile(String filename) throws ParserConfigurationException {
+  public Document getDocFromFile(String filename) throws ParserConfigurationException {
     {
       DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
       DocumentBuilder db= dbf.newDocumentBuilder();
@@ -30,10 +30,9 @@ public class BoardImport {
 
   }
 
-  public static Room[] readBoardData(Document d) {
+  public Room[] readBoardData(Document d) {
     Element root = d.getDocumentElement();
     NodeList setList = root.getElementsByTagName("set");
-    Role[] roles = new Role[4];
     Room[] rooms = new Room[10];
     int takeNum = 0;
     for (int i=0; i<setList.getLength(); i++) {
@@ -42,95 +41,90 @@ public class BoardImport {
 
       String setName = set.getAttributes().getNamedItem("name").getNodeValue();
 
-//neighbors
-
+      //neighbors
       Element setNeighbors = (Element)set;
       Element neighborE = (Element)setNeighbors.getElementsByTagName("neighbors").item(0);
 
-
-//neighbor
-      NodeList neighborList = root.getElementsByTagName("neighbor");
+      //neighbor
+      NodeList neighborList = neighborE.getElementsByTagName("neighbor");
       String[] neighborNames = new String[neighborList.getLength()];
       for (int j=0; j<neighborList.getLength(); j++) {
         Node neighbor = neighborList.item(j);
         String neighborName = neighbor.getAttributes().getNamedItem("name").getNodeValue();
         neighborNames[j] = neighborName;
+        //System.out.println(neighborName);
+      }
+      //System.out.println();
+
+      //done w/ neighbors
+      //area
+      Element setArea = (Element)set;
+      Element areaE = (Element)setArea.getElementsByTagName("area").item(0);
+      int areaX = Integer.parseInt(areaE.getAttributes().getNamedItem("x").getNodeValue());
+      int areaY = Integer.parseInt(areaE.getAttributes().getNamedItem("y").getNodeValue());
+      int areaH = Integer.parseInt(areaE.getAttributes().getNamedItem("h").getNodeValue());
+      int areaW = Integer.parseInt(areaE.getAttributes().getNamedItem("w").getNodeValue());
+      //System.out.println(areaX + " " + areaY+ " " + areaH + " " + areaW+"\n");
+      //done w/ area
+
+      //takes
+      Element setTakes = (Element)set;
+      Element takesE = (Element)setTakes.getElementsByTagName("takes").item(0);
+
+      //take
+      NodeList takeList = takesE.getElementsByTagName("take");
+
+      Node take = takeList.item(0);
+      takeNum = takeList.getLength();
+
+      Element takeArea = (Element)take;
+      Element takeareaE = (Element)takeArea.getElementsByTagName("area").item(0);
+      int takeareaX = Integer.parseInt(takeareaE.getAttributes().getNamedItem("x").getNodeValue());
+      int takeareaY = Integer.parseInt(takeareaE.getAttributes().getNamedItem("y").getNodeValue());
+      int takeareaH = Integer.parseInt(takeareaE.getAttributes().getNamedItem("h").getNodeValue());
+      int takeareaW = Integer.parseInt(takeareaE.getAttributes().getNamedItem("w").getNodeValue());
+      //System.out.println(takeareaX + " " + takeareaY+ " " + takeareaH + " " + takeareaW+"\n");
+
+      //done w/ takes
+      //parts
+
+      Element setParts = (Element)set;
+      Element partE = (Element)setParts.getElementsByTagName("part").item(0);
+
+
+      //part
+      NodeList partList = root.getElementsByTagName("part");
+      Role[] roles = new Role[partList.getLength()];
+
+      for (int b=0; b<partList.getLength(); b++) {
+        Node part = partList.item(b);
+        String partName = part.getAttributes().getNamedItem("name").getNodeValue();
+        int partLevel = Integer.parseInt(part.getAttributes().getNamedItem("level").getNodeValue());
+
+        Element partArea = (Element)part;
+        Element partareaE = (Element)partArea.getElementsByTagName("area").item(0);
+        int partareaX = Integer.parseInt(partareaE.getAttributes().getNamedItem("x").getNodeValue());
+        int partareaY = Integer.parseInt(partareaE.getAttributes().getNamedItem("y").getNodeValue());
+        int partareaH = Integer.parseInt(partareaE.getAttributes().getNamedItem("h").getNodeValue());
+        int partareaW = Integer.parseInt(partareaE.getAttributes().getNamedItem("w").getNodeValue());
+        //System.out.println(partareaX + " " + partareaY+ " " + partareaH + " " + partareaW+"\n");
+
+        Element partD = (Element)part;
+        String partLine = partD.getElementsByTagName("line").item(0).getTextContent();
+        Role r = new Role(partName, partLine, partLevel, false);
+        roles[b] = r;
+
       }
 
-
-//done w/ neighbors
-//area
-System.out.println(setName + " " + neighborList+ "\n");
-Element setArea = (Element)set;
-Element areaE = (Element)setArea.getElementsByTagName("area").item(0);
-int areaX = Integer.parseInt(areaE.getAttributes().getNamedItem("x").getNodeValue());
-int areaY = Integer.parseInt(areaE.getAttributes().getNamedItem("y").getNodeValue());
-int areaH = Integer.parseInt(areaE.getAttributes().getNamedItem("h").getNodeValue());
-int areaW = Integer.parseInt(areaE.getAttributes().getNamedItem("w").getNodeValue());
-System.out.println(areaX + " " + areaY+ " " + areaH + " " + areaW+"\n");
-//done w/ area
-
-//takes
-Element setTakes = (Element)set;
-Element takesE = (Element)setTakes.getElementsByTagName("takes").item(0);
-
-//take
-
-NodeList takeList = root.getElementsByTagName("take");
-
-
-  Node take = takeList.item(0);
-  takeNum = takeList.getLength();
-
-  Element takeArea = (Element)take;
-  Element takeareaE = (Element)takeArea.getElementsByTagName("area").item(0);
-  int takeareaX = Integer.parseInt(takeareaE.getAttributes().getNamedItem("x").getNodeValue());
-  int takeareaY = Integer.parseInt(takeareaE.getAttributes().getNamedItem("y").getNodeValue());
-  int takeareaH = Integer.parseInt(takeareaE.getAttributes().getNamedItem("h").getNodeValue());
-  int takeareaW = Integer.parseInt(takeareaE.getAttributes().getNamedItem("w").getNodeValue());
-  System.out.println(takeareaX + " " + takeareaY+ " " + takeareaH + " " + takeareaW+"\n");
-
-
-
-
-//done w/ takes
-//parts
-
-Element setParts = (Element)set;
-Element partE = (Element)setParts.getElementsByTagName("part").item(0);
-
-
-//part
-NodeList partList = root.getElementsByTagName("part");
-for (int b=0; b<partList.getLength(); b++) {
-  Node part = partList.item(b);
-  String partName = part.getAttributes().getNamedItem("name").getNodeValue();
-  int partLevel = Integer.parseInt(part.getAttributes().getNamedItem("level").getNodeValue());
-
-  Element partArea = (Element)part;
-  Element partareaE = (Element)partArea.getElementsByTagName("area").item(0);
-  int partareaX = Integer.parseInt(partareaE.getAttributes().getNamedItem("x").getNodeValue());
-  int partareaY = Integer.parseInt(partareaE.getAttributes().getNamedItem("y").getNodeValue());
-  int partareaH = Integer.parseInt(partareaE.getAttributes().getNamedItem("h").getNodeValue());
-  int partareaW = Integer.parseInt(partareaE.getAttributes().getNamedItem("w").getNodeValue());
-  //System.out.println(partareaX + " " + partareaY+ " " + partareaH + " " + partareaW+"\n");
-
-  Element partD = (Element)part;
-  String partLine = partD.getElementsByTagName("line").item(0).getTextContent();
-  Role r = new Role(partName, partLine, partLevel, false);
-  roles[b] = r;
-
-}
-
-  Room r = new Room(setName, neighborNames, takeNum, roles);
-System.out.println(setName+" "+neighborNames[0]);
-  rooms[i] = r;
+        Room r = new Room(setName, neighborNames, takeNum, roles);
+      //System.out.println(setName + " " + neighborNames[0]+ " " + takeNum);
+        rooms[i] = r;
 
     }
-return rooms;
+    return rooms;
   }
 
-  public static void main(String[] args) {
+  public Room[] getRooms(String[] args) {
     Document doc = null;
     try{
       doc = getDocFromFile("board.xml");
@@ -138,7 +132,7 @@ return rooms;
     } catch (Exception ex){
     }
     Room[] roomArray = readBoardData(doc);
-    //return roomArray;
+    return roomArray;
   }
 
 }
