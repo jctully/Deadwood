7b1585af3e8457b4b5b34af24a528367919f18e0
Joseph Tully <tullyj2@wwu.edu>
2018-05-17 19:56:09 -0700

    added more player options

--- Deadwood.java
+++ Deadwood.java
@@ -8,16 +8,15 @@ public class Deadwood {
   Board board;
   */
 
-  public Player getCurrPlayer(Player[] players, int currentPlayerNum) {
-    return players[currentPlayerNum];
-  }
-
-  public Player nextTurn(Player[] players, int currentPlayerNum) {
+  public static int nextTurn(Player[] players, int currentPlayerNum) {
+    Player current = players[currentPlayerNum];
+    current.resetMovedThisTurn();
     currentPlayerNum = (currentPlayerNum+1)%players.length;
-    return players[currentPlayerNum];
+    System.out.println("Player " + (currentPlayerNum+1) + "'s turn\n");
+    return currentPlayerNum;
   }
 
-  public void wrap(Room room, Board board, Player[] players) {
+  public static void wrap(Room room, Board board, Player[] players) {
     room.wrap();
     board.wrapRoom();
     if (room.getCard().hasOnCard()) {//need to give rewards
@@ -32,7 +31,7 @@ public class Deadwood {
     }
   }
 
-  public void endDay(Player[] players) {
+  public static void endDay(Player[] players) {
     //Board.clearBoard();
     for (Player p : players) {
       // p.move(trailer);
@@ -40,7 +39,7 @@ public class Deadwood {
     }
   }
 
-  public void endGame() {
+  public static void endGame() {
     //calc score
   }
 
@@ -48,76 +47,134 @@ public class Deadwood {
     int currentDay = 0;
     int numPlayers = Integer.parseInt(args[0]);
     Player[] players = new Player[numPlayers];
-
     int currentPlayerNum = 0;
     Board board = new Board();
     board.initRooms();
     for (int i =0; i<numPlayers; i++) {
       players[i] = new Player(i, board.findRoom("trailer"));
     }
-    Player currentPlayer = players[0];
+
     Scanner scn = new Scanner(System.in);
 
-    //while (day != 4) {
     System.out.print("Day " + currentDay + ", ");
+    System.out.println("Player " + (currentPlayerNum+1) + "'s turn");
 
-    System.out.println("Player " + currentPlayerNum+ "'s turn");
+    while (currentDay != 4) {
+    Player currentPlayer = players[currentPlayerNum];
+
     System.out.println("Options:");
-    System.out.println("1 - Show all player locations");
-    System.out.println("2 - Move");
-    System.out.println("3 - Take roll");
-    System.out.println("4 - Act");
-    System.out.println("5 - Rehearse");
-    System.out.println("6 - Upgrade");
-    System.out.println("7 - End Turn");
-    System.out.println("Input the number pertaining to the action you would like to preform.");
+    System.out.println("1 - Current Player Info");
+    System.out.println("2 - Show all player locations");
+    System.out.println("3 - Move");
+    System.out.println("4 - Take role");
+    System.out.println("5 - Act");
+    System.out.println("6 - Rehearse");
+    System.out.println("7 - Upgrade");
+    System.out.println("8 - End Turn");
+    System.out.println("\nInput the number pertaining to the action you would like to perform.");
 
     int i = scn.nextInt();
-    if (i == 1) {
+    if (i == 1) {//player info
+      System.out.println("ID: " + (currentPlayer.getPlayerID()+1));
+      System.out.println("Rank: " + currentPlayer.getRank());
+      System.out.println("Money: " + currentPlayer.getMoney());
+      System.out.println("Fame Points: " + currentPlayer.getFame());
+      System.out.println("Location: " + currentPlayer.getPlayerLoc().getTitle());
+      System.out.print("Working: ");
+      if (currentPlayer.getWorkStatus()) {
+        System.out.println(currentPlayer.getRole().getTitle() + ", " + currentPlayer.getRehearseBonus() + " rehearse tokens.");
+      }
+      else {
+        System.out.println("Not working.");
+      }
+      System.out.println();
+
+    }
+
+    if (i == 2) {//display locations
       for (Player player : players) {
-        System.out.println(currentPlayer.getPlayerID() + " " +currentPlayer.getPlayerLoc().getTitle());
+        System.out.println("Player " + player.getPlayerID()+1 + " in " +player.getPlayerLoc().getTitle());
       }
-    }else if (i == 2) {
-      System.out.println("Input the number pertaining to the room you would like to move to.");
-      String[] options = currentPlayer.getPlayerLoc().getAdjacentRooms();
-      for (int a=1; a<=options.length; a++) {
-        System.out.println(a+ " - " + options[a-1]);
-      }
-
-    }else if (i == 3) {
-      int playerRank=currentPlayer.getRank();
-      Role[] onCard = currentPlayer.getPlayerLoc().getCard().getRoles();
-      Role[] offCard = currentPlayer.getPlayerLoc().getRoles();
-
-      System.out.println("Your rank is"+ playerRank);
-
-      int possibleRoles = onCard.length + offCard.length;
-      for (int a=1; a-1< possibleRoles; a++) {
-
-        for (Role role : offCard ) {
-          System.out.println(a++ +" - "+role.getTitle()+ " " +role.getRank());
+    }else if (i == 3) {//move
+      if (!currentPlayer.hasMovedThisTurn()) {
+        System.out.println("\nInput the number pertaining to the room you would like to move to.");
+        String[] options = currentPlayer.getPlayerLoc().getAdjacentRooms();
+        for (int a=1; a<=options.length; a++) {
+          System.out.println(a+ " - " + options[a-1]);
         }
-        for (Role role : onCard ) {
-          System.out.println(a++ +" - "+role.getTitle()+ " " +role.getRank());
-        }
+        int opt = scn.nextInt();
+        currentPlayer.move(board.findRoom(options[opt-1]));
+        System.out.println("Moved to " + currentPlayer.getPlayerLoc().getTitle());
+        System.out.println("----------");
+      }
+      else{//already moved
+        System.out.println("You have already moved this turn\n");
       }
 
-      System.out.println("The off card roles available in this room are:" + currentPlayer.getPlayerLoc().getRoles());
-      System.out.println("The on card roles available in this room are:" + currentPlayer.getPlayerLoc().getCard().getRoles());
+
+    }else if (i == 4) {//take role
+      if (currentPlayer.getPlayerLoc().isWrapped()) {
+        System.out.println("Current room is wrapped. Unable to act.");
+      }
+      else if (currentPlayer.workStatus == true) {
+        System.out.println("You are already working a role.");
+      }
+      else {
+        Boolean success = false;
+        int playerRank=currentPlayer.getRank();
+        Role[] onCard = currentPlayer.getPlayerLoc().getCard().getRoles();
+        Role[] offCard = currentPlayer.getPlayerLoc().getRoles();
+        int possibleRoles = onCard.length + offCard.length;
+
+        Role[] allRoles = new Role[possibleRoles];
+        for (int j=0; j<possibleRoles; j++) {
+          for (Role role : offCard )
+            allRoles[j++] = role;
+          for (Role role : onCard )
+            allRoles[j++] = role;
+        }
+
+        while(!success) {
+          System.out.println("\nYour rank is "+ playerRank);
+          System.out.println("0 - return to menu");
+          for (int a=0; a< possibleRoles; a++) {
+            String onCardStr = null;
+            if(allRoles[a].isOnCard())
+              onCardStr = " On Card";
+            else onCardStr = " Off Card";
+            System.out.println((a+1) +" - "+ allRoles[a].getTitle()+ ", Rank = "
+              + allRoles[a].getRank() + ", " + onCardStr);
+          }
+          int opt = scn.nextInt();
+          if (opt == 0) {
+            break;
+          }
+          if (currentPlayer.checkRole(allRoles[opt-1])) {
+            currentPlayer.takeRole(allRoles[opt-1]);
+            success = true;
+            currentPlayerNum = nextTurn(players, currentPlayerNum);
+          }
+
+        }//end while
+
+      }//end else
+
+      //System.out.println("The off card roles available in this room are:" + currentPlayer.getPlayerLoc().getRoles());
+      //System.out.println("The on card roles available in this room are:" + currentPlayer.getPlayerLoc().getCard().getRoles());
       //System.out.println("The off card roles available in this room are:" + player.getPlayerLoc.getRoles());
-    }else if (i == 4) {
-
     }else if (i == 5) {
 
     }else if (i == 6) {
 
     }else if (i == 7) {
 
+    }else if (i == 8) {
+      currentPlayerNum = nextTurn(players, currentPlayerNum);
     }
 
 
 
-
+  }
 
 
 
