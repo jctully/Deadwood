17d82f020159681d328d0d5f90c3ecd1f773f834
James Riley <rileyj20@LabImage2016-17>
2018-05-18 21:21:53 -0700

    added reverse methods

--- RNG.java
+++ RNG.java
@@ -15,4 +15,25 @@ public class RNG {
     Arrays.sort(rolls, 0, budget);
     return rolls;
   }
+
+  public int[] reverse(int[] r){
+    for(int i = 0; i < r.length / 2; i++)
+    {
+    int temp = r[i];
+    r[i] = r[r.length - i - 1];
+    r[r.length - i - 1] = temp;
+}
+return r;
+}
+
+public Role[] reverse(Role[] r){
+  for(int i = 0; i < r.length / 2; i++)
+  {
+  Role temp = r[i];
+  r[i] = r[r.length - i - 1];
+  r[r.length - i - 1] = temp;
+}
+return r;
+}
+
 }
