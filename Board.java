e57434782689f676fb7d49c2f695d9047744c753
Joseph Tully <tullyj2@wwu.edu>
2018-05-18 20:14:04 -0700

    bug fix

--- Board.java
+++ Board.java
@@ -26,13 +26,14 @@ class Board
    }
 
    public void initRooms(){
+     this.roomsLeft = 10;
      CardImport cardImport = new CardImport();
      Card[] cards = cardImport.getCards();
-     for (Room r : this.roomList) {
+     for (Room r : this.roomList) {//randomize card dealt to each room
        RNG rng = new RNG();
        int cardIndex = rng.getRandomNum(0,cards.length-1);
        Card card = cards[cardIndex];
-       while (card.isOnBoard()) {//card has been dealt on board
+       while (card.isOnBoard()) {//card has been dealt on board already
          cardIndex = rng.getRandomNum(0,cards.length-1);
          card = cards[cardIndex];
        }
